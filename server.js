d0442cd4edef9c51418b80a70b71959893277017
Karankuntal <karan7819961859@gmail.com>
2026-04-07 12:30:39 +0530

    Refactor server.js for improved structure

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.post('/delete', (req, res) => {
     res.send('Deleted');
 });
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
