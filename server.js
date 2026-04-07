ddbf8d478ac735846114b9f78229de56f8dd2b3a
Karankuntal <karan7819961859@gmail.com>
2026-04-07 11:50:12 +0530

    Configure server to use environment variable for port
    Added support for dynamic port configuration.

--- server.js
+++ server.js
@@ -56,3 +56,5 @@ app.post('/delete', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
